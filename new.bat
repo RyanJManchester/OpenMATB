01ef1078e83ecdaea3cff398f15907d8dd61ea39
Manchester <rm534@students.waikato.ac.nz>
2025-11-21 11:20:49 +1300

    updating

--- new.bat
+++ new.bat
@@ -19,22 +19,17 @@ if not errorlevel 1 goto wait_for_firefox1
 
 REM 3) MAIN TASK / FATIGUE INDUCTION BLOCK (Python "start")
 cd start
-py -3.9 main.py
+py -3.9 main.py   
 cd ..
-
-REM 4) SECOND PVT (repeat response test)
-"%FIREFOX%" "%CD%\%HTML%"
-
-REM 5) Wait for all Firefox processes to exit
-echo Waiting for Firefox to close (PVT 2)...
-:wait_for_firefox2
-timeout /t 1 /nobreak >nul
-tasklist /FI "IMAGENAME eq firefox.exe" | find /I "firefox.exe" >nul
-if not errorlevel 1 goto wait_for_firefox2
 
 REM 6) FINAL CLEANUP / END BLOCK (Python "end")
 cd end
 py -3.9 main.py
 cd ..
+
+
+REM 4) SECOND PVT (repeat response test)
+"%FIREFOX%" "%CD%\%HTML%"
+
 
 endlocal
